3d7f7f021e1bce0169fd749cf3ac17f5f5d73a22
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2022-03-04 00:12:25 -0800

    fix auth

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -14,19 +14,8 @@ const SignIn = (props) => {
   return (
     <div className="login">
       <h1>Create New Account</h1>
-      {/* <form>
-        <TextField id="name" label="Username" margin="normal" />
-        <TextField fullWidth id="Email" label="Email" margin="normal" />
-        <TextField id="password" label="Password" margin="normal" />
-        <TextField
-          id="confirmPassword"
-          label="Confirm Password"
-          margin="normal"
-        />
-      </form> */}
       <div className="buttons">
         <Button onClick={signInHandler}>Sign Up</Button>
-        {/* <Button onClick={signInHandler}>Use Google</Button> */}
       </div>
     </div>
   );
